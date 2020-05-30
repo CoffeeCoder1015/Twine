7ada79b667736b7a80b60fb53d4a272a15d60112
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-05-30 22:42:16 +1200

    master

--- tw_main.py
+++ tw_main.py
@@ -1,21 +1,11 @@
 import os
 
-import ATM
 from scan_py import scan
 
-SCAN = scan(Name='',matchMode='',searchType='')
+SCAN = scan(Name='',matchMode='',searchType='d')
 SCAN.scan_mc(path="..")
 
-#cache init
-cacher = ATM.cache_machine(name="Twine",start_pos=os.getcwd())
 path=SCAN.path
 rls =SCAN.rls
 
-rls = list(str(rls))
-del rls[0]
-del rls[len(rls)-1]
-rls="".join(rls).replace("'","").replace("], [","]\n[")
-
-cacher.deposit(data=rls,name=path,cacheTarget="twine_cache")
-print(cacher.withdraw(name=path,cacheTarget="twine_cache"))
-cacher.clear(cacheTarget="twine_cache")
+print(rls.__sizeof__())
