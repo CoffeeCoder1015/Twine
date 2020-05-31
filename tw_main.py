56cbc20419a85474ec3f48e135ce4b024a6e7b2c
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-06-01 09:09:54 +1200

    master

--- tw_main.py
+++ tw_main.py
@@ -1,10 +1,7 @@
 from scan_py import scan
-import timer
 
-timer.start()
 SCAN = scan(Name='', matchMode='', searchType='')
 SCAN.scan_mc(path="..")
 rlsc = SCAN.rls
 print(SCAN.path)
 print(len(rlsc))
-timer.end()
