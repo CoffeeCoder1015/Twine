3b41e81d14aa94b9ec22f2a889205808c216aadd
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2020-06-01 15:21:45 +1200

    concurrent-verison

--- tw_main.py
+++ tw_main.py
@@ -1,10 +1,8 @@
-import os
-
 from scan_py import scan
 
-SCAN = scan(Name='',matchMode='',searchType='')
-#SCAN.scan_mc(path="..\\..")
-SCAN.scanner(path=".")
-rls =SCAN.rls
-for i in rls:
-    print(i)
+timer.start()
+SCAN = scan(Name='', matchMode='', searchType='')
+SCAN.scan_mc(path=".")
+rlsc = SCAN.rls
+print(SCAN.path)
+print(len(rlsc))
