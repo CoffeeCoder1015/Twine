4867eb9b6bf9424c379859b7051558eb2f2403eb
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-15 05:48:46 +1000

    UI setup

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
-
-    "twine/twine"
+    tea "github.com/charmbracelet/bubbletea"
 )
+
+
+type model struct{
+
+}
+
+func (m model) Init() tea.Cmd {
+    // Just return `nil`, which means "no I/O right now, please."
+    return nil
+}
+
+// Called to render UI
+func (m model) View() string{
+}
+
+
+// Handles state updates like key inputs
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Msg){
+
+}
 
 func main() {
     fmt.Println("Hello world")
