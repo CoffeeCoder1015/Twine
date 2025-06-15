24e2f57aba6f4b09cacf77febe6f902300620248
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-16 02:48:06 +1000

    results list

---
+++ resultmodel.go
@@ -0,0 +1,24 @@
+package main
+import (
+    tea "github.com/charmbracelet/bubbletea"
+)
+
+type ResultsList struct{
+
+}
+
+func InitResults() ResultsList{
+
+}
+
+func (m ResultsList) Init() tea.Cmd{
+    return nil
+}
+
+func (m ResultsList) Update(msg tea.Msg) (tea.Model,tea.Cmd){
+
+}
+
+func (m ResultsList) View() string{
+
+}
