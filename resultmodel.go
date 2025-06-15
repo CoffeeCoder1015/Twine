b03bb4be56e812cc852c05d51437806e664c5b9e
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-16 03:07:35 +1000

    list setup

--- resultmodel.go
+++ resultmodel.go
@@ -1,14 +1,43 @@
 package main
+
 import (
-    tea "github.com/charmbracelet/bubbletea"
+	"regexp"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type ResultsList struct{
+var ( 
+    docStyle = lipgloss.NewStyle().Margin(1, 2) 
+)
 
+type queryFilterPattern struct{
+    directory string
+    name *regexp.Regexp
+    fileSize string
+    mode *regexp.Regexp
+    date string
+    DirFile string
+}
+
+type item struct{
+    title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type ResultsList struct{
+    list list.Model
+    filter queryFilterPattern
 }
 
 func InitResults() ResultsList{
-
+    return ResultsList{
+        filter: queryFilterPattern{},
+    }
 }
 
 func (m ResultsList) Init() tea.Cmd{
@@ -16,9 +45,17 @@ func (m ResultsList) Init() tea.Cmd{
 }
 
 func (m ResultsList) Update(msg tea.Msg) (tea.Model,tea.Cmd){
+    	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
 
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m ResultsList) View() string{
-
+    return docStyle.Render(m.list.View())
 }
