048c3a5708b6886ec302c3162ba412f724780551
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-06-16 15:32:20 +1000

    index capture

--- resultmodel.go
+++ resultmodel.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -21,6 +23,7 @@ func (i item) FilterValue() string { return i.title }
 type ResultsList struct{
     list list.Model
     twine Twine
+    index int64
 }
 
 func InitResults() ResultsList{
@@ -46,8 +49,20 @@ func (m ResultsList) Update(msg tea.Msg) (ResultsList,tea.Cmd){
     case tea.WindowSizeMsg:
         h, v := docStyle.GetFrameSize()
         m.list.SetSize(msg.Width-h, msg.Height-v-12)
+    case tea.KeyMsg:
+        switch msg.String(){
+        case "down", "j":
+            m.index++
+        case "up", "k":
+            m.index--
+        case "right","l", "pgdown", "f", "d":
+            m.index += int64(m.list.Paginator.PerPage) 
+        case "left","h", "pgup", "b" ,"u":
+            m.index -= int64(m.list.Paginator.PerPage) 
+        }
     }
 
+    m.list.Title =  fmt.Sprintf("Results %d",m.index)
     var cmd tea.Cmd
     m.list, cmd = m.list.Update(msg)
     return m, cmd
